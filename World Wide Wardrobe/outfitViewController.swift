3bdb6a5fad18b68295c3855aba8a48aed2c0a18d
Robert McGinness <RMcGinness@roberts-macbook-air-2.local>
2016-04-07 12:09:17 -0500

    added view controller files, custom class, and placeholder image

---
+++ World Wide Wardrobe/outfitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  outfitViewController.swift
+//  World Wide Wardrobe
+//
+//  Created by Robert McGinness on 4/7/16.
+//  Copyright © 2016 $wag Productions. All rights reserved.
+//
+
+import UIKit
+
+class outfitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ clothingItem.swift
@@ -0,0 +1,20 @@
+//
+//  clothingItem.swift
+//  World Wide Wardrobe
+//
+//  Created by Robert McGinness on 4/7/16.
+//  Copyright © 2016 $wag Productions. All rights reserved.
+//
+
+import UIKit
+
+class clothingItem: NSObject {
+    var name = ""
+    var image = UIImage(named: "placeholder")
+    var clothingItemType = 0
+    //0 is shirt
+    //1 is pants
+    //2 is shoes
+    var clothingType = ""   //types such as "Formalwear", "Casualwear", etc.
+
+}
