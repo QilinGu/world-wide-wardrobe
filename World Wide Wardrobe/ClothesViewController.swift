1834ff99f23e84837c207d41080fcf6a80969964
Grace Knox <gknox@graces-macbook-air-2.local>
2016-04-14 12:39:35 -0500

    -started allowing clothes to be saved in clothesArray

--- World Wide Wardrobe/ClothesViewController.swift
+++ World Wide Wardrobe/ClothesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SafariServices
 
-class ClothesViewController: UIViewController, SFSafariViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ClothesViewController: UIViewController, UITableViewDataSource, SFSafariViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
     var clothes = clothingItem(clothingItemImage: UIImage(named: "placeholder")!)
@@ -31,6 +31,33 @@ class ClothesViewController: UIViewController, SFSafariViewControllerDelegate, U
         }
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clothesArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("My Cell", forIndexPath: indexPath)
+       cell.textLabel?.text = clothesArray[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+        clothesArray.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+       let clothes = clothesArray[sourceIndexPath.row]
+       clothesArray.removeAtIndex(sourceIndexPath.row)
+       clothesArray.insert(clothes, atIndex: destinationIndexPath.row)
+    }
+    
     //allows user to go to library
     @IBAction func onTappedLibraryButton(sender: AnyObject) {
         imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
@@ -45,7 +72,7 @@ class ClothesViewController: UIViewController, SFSafariViewControllerDelegate, U
         }
     }
     
-    @IBAction func onTappedSaveButton(sender: AnyObject) {
+    @IBAction func onTappedSave(sender: AnyObject) {
         let alert = UIAlertController(title: "What is this item?", message: nil, preferredStyle: .Alert)
         alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
             textField.placeholder = "Add Item"
@@ -54,7 +81,7 @@ class ClothesViewController: UIViewController, SFSafariViewControllerDelegate, U
         alert.addAction(cancelAction)
         let addAction = UIAlertAction(title: "Save", style: .Default) { (action) -> Void in
             
-           }
+        }
         
         alert.addAction(addAction)
         self.presentViewController(alert, animated: true, completion: nil)
