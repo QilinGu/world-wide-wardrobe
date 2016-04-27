6fb841e26648009e06e9753d160ebc5dbdbc3efe
Robert McGinness <RMcGinness@roberts-macbook-air-2.local>
2016-04-27 12:16:31 -0500

    added table view in ClothesViewController

--- World Wide Wardrobe/ClothesViewController.swift
+++ World Wide Wardrobe/ClothesViewController.swift
@@ -9,111 +9,30 @@
 import UIKit
 import SafariServices
 
-class ClothesViewController: UIViewController, UITableViewDataSource, SFSafariViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ClothesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
-    let imagePicker = UIImagePickerController()
     var clothes = clothingItem(clothingItemImage: UIImage(named: "placeholder")!)
     var clothesArray : [clothingItem] = []
     var outfitsArray : [outfitObject] = []
     var clothesImages : outfitObject?
-    
-    var pickerFrame: CGRect = CGRectMake(17, 52, 270, 100)
-    
-    @IBOutlet weak var clothingItemImageView: UIImageView!
-    @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    @IBOutlet weak var clothesTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        clothingItemImageView.image = clothes.clothingItemImage
-        imagePicker.delegate = self
         
             }
     
-    
-    //puts pictures of clothes into clothingItemImageView
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        imagePicker.dismissViewControllerAnimated(true) { () -> Void in
-            let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-            self.clothingItemImageView.image = selectedImage
-        }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = clothesTableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = clothesArray[indexPath.row].name
+        return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return clothesArray.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("My Cell", forIndexPath: indexPath)
-       cell.textLabel?.text = clothesArray[indexPath.row].clothingType
-        return cell
-    }
     
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-        clothesArray.removeAtIndex(indexPath.row)
-            tableView.reloadData()
-        }
-    }
-    
-    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
-       let clothes = clothesArray[sourceIndexPath.row]
-       clothesArray.removeAtIndex(sourceIndexPath.row)
-       clothesArray.insert(clothes, atIndex: destinationIndexPath.row)
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if saveButton === sender {
-            let clothesImages = clothingItemImageView.image
-        }
-        clothesImages = outfitObject(clothingType: "") 
-    }
-    
-    //allows user to go to library
-    @IBAction func onTappedLibraryButton(sender: AnyObject) {
-        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        presentViewController(imagePicker, animated: true, completion: nil)
-    }
-    
-    //allows user to go to camera
-    @IBAction func onTappedCameraButton(sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
-            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-            presentViewController(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
-
-    //define what type of clothing item the image is
-    @IBAction func nameButton(sender: AnyObject) {
-        self.outfitsArray.append(outfitObject.init(self.clothes.clothingItemImage = self.clothingItemImageView.image))
-
-        let alert = UIAlertController(title: "What is this item?", message: nil, preferredStyle: .Alert)
-        alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
-            textField.placeholder = "Add Item"
-            textField.text = self.clothes.name
-            self.clothes.clothingItemImage = self.clothingItemImageView.image!
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        alert.addAction(cancelAction)
-        
-        let addAction = UIAlertAction(title: "Save", style: .Default) { (action) -> Void in
-            
-        } 
-        
-        alert.addAction(addAction)
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
-    @IBAction func unwindToClothesArray(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.sourceViewController as? ClothesViewController, clothesImages = sourceViewController.clothesImages {
-            let newIndexPath = NSIndexPath(forRow: clothesArray.count, inSection: 0)
-            outfitsArray.append(clothesImages)
-        }
-    }
     
 }
