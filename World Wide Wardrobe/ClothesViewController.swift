b5240342aefd654ac1b640f2c8c8ef1600222b24
Robert McGinness <RMcGinness@roberts-macbook-air-2.local>
2016-04-29 12:20:34 -0500

    added table view back to storyboard

--- World Wide Wardrobe/ClothesViewController.swift
+++ World Wide Wardrobe/ClothesViewController.swift
@@ -16,7 +16,7 @@ class ClothesViewController: UIViewController, UITableViewDataSource, UITableVie
     var outfitsArray : [outfitObject] = []
     var clothesImages : outfitObject?
 
-    @IBOutlet weak var clothesTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ class ClothesViewController: UIViewController, UITableViewDataSource, UITableVie
             }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = clothesTableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
         cell.textLabel?.text = clothesArray[indexPath.row].name
         cell.imageView?.image = clothesArray[indexPath.row].image
         return cell
@@ -32,7 +32,7 @@ class ClothesViewController: UIViewController, UITableViewDataSource, UITableVie
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-        clothesArray.removeAtIndex(indexPath.row)
+            clothesArray.removeAtIndex(indexPath.row)
             tableView.reloadData()
         }
     }
@@ -41,9 +41,6 @@ class ClothesViewController: UIViewController, UITableViewDataSource, UITableVie
       return true
     }
     
-    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
     
     func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
        let clothes = clothesArray[sourceIndexPath.row]
@@ -51,62 +48,10 @@ class ClothesViewController: UIViewController, UITableViewDataSource, UITableVie
        clothesArray.insert(clothes, atIndex: destinationIndexPath.row)
     }
     
-    
-    //allows user to go to library
-    @IBAction func onTappedLibraryButton(sender: AnyObject) {
-        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        presentViewController(imagePicker, animated: true, completion: nil)
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clothesArray.count
     }
-    
-    //allows user to go to camera
-    @IBAction func onTappedCameraButton(sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
-            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-            presentViewController(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
 
-    //define what type of clothing item the image is
-    @IBAction func nameButton(sender: AnyObject) {
-        let alert = UIAlertController(title: "Add Clothes", message: nil, preferredStyle: .Alert)
-        alert.addTextFieldWithConfigurationHandler { (nameTextField) -> Void in
-            nameTextField.placeholder = "Name"
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
-        alert.addAction(cancelAction)
-        let addTopAction = UIAlertAction(title: "Add Top", style: .Default) { (action) -> Void in
-            let nameTextField = alert.textFields![0] as UITextField
-            self.clothesArray.append(clothingItem(name: nameTextField.text!,image: UIImage(named: "placeholder")!))
-          //  self.shirtImagesArray.append(self.clothesArray[self.clothesArray.count - 1].image!)
-         //   self.checkArrowAlpha()
-            
-        }
-        alert.addAction(addTopAction)
-        let addBottomAction = UIAlertAction(title: "Add Bottom", style: .Default) { (action) -> Void in
-            let nameTextField = alert.textFields![0] as UITextField
-            self.clothesArray.append(clothingItem(name: nameTextField.text!,image: UIImage(named: "placeholder4")!))
-         //   self.shirtImagesArray.append(self.clothesArray[self.clothesArray.count - 1].image!)
-         //   self.checkArrowAlpha()
-        }
-        alert.addAction(addBottomAction)
-        
-        let addShoeAction = UIAlertAction(title: "Add Shoes", style: .Default) { (action) -> Void in
-            let nameTextField = alert.textFields![0] as UITextField
-            self.clothesArray.append(clothingItem(name: nameTextField.text!,image: UIImage(named: "placeholder7")!))
-         //   self.shirtImagesArray.append(self.clothesArray[self.clothesArray.count - 1].image!)
-         //   self.checkArrowAlpha()
-            
-        }
-        alert.addAction(addShoeAction)
-        
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
     
-    @IBAction func unwindToClothesArray(sender: UIStoryboardSegue) {
-        if let sourceViewController = sender.sourceViewController as? ViewController, clothesImages = sourceViewController.clothesImages {
-            outfitsArray.append(clothesImages)
-        }
-    }
     
 }
