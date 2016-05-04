6622dc17f389598c4f19af561083cafa5dcd7cb0
Sabrina Chung <student@bluc02mf2w5fh51.local>
2016-05-04 12:21:47 -0500

    Detail View Controller outlets

---
+++ DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  World Wide Wardrobe
+//
+//  Created by Sabrina Chung on 5/4/16.
+//  Copyright © 2016 $wag Productions. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var shoesImage: UIImageView!
+    @IBOutlet var bottomImage: UIImageView!
+    @IBOutlet var topImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
